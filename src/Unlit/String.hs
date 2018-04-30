d102dbc666206d7d43a6d30cc84104c026c06fa5
Daniel Mendler <mail@daniel-mendler.de>
2018-04-30 12:25:41 +0200

    add asciidoc support
    Code blocks have the form:
    
    [source,lang]
    ----
    ....
    ----

--- src/Unlit/String.hs
+++ src/Unlit/String.hs
@@ -22,11 +22,12 @@ stripEnd   = dropWhileEnd Char.isSpace
 toLower    = map Char.toLower
 
 data Delimiter
-  = LaTeX         BeginEnd
-  | OrgMode       BeginEnd Lang
+  = LaTeX    BeginEnd
+  | OrgMode  BeginEnd Lang
   | Bird
-  | Jekyll        BeginEnd Lang
-  | Markdown      Fence Lang
+  | Jekyll   BeginEnd Lang
+  | Markdown Fence Lang
+  | Asciidoc BeginEnd Lang
   deriving (Eq, Show)
 
 data BeginEnd
@@ -35,17 +36,19 @@ data BeginEnd
   deriving (Eq, Show)
 
 isBegin :: Delimiter -> Bool
-isBegin (LaTeX   Begin  ) = True
-isBegin (OrgMode Begin _) = True
-isBegin (Jekyll  Begin _) = True
-isBegin (Markdown _ _)    = True
-isBegin  _                = False
+isBegin (LaTeX    Begin  ) = True
+isBegin (OrgMode  Begin _) = True
+isBegin (Jekyll   Begin _) = True
+isBegin (Asciidoc Begin _) = True
+isBegin (Markdown _ _)     = True
+isBegin  _                 = False
 
 setBegin :: BeginEnd -> Delimiter -> Delimiter
-setBegin beginEnd (LaTeX   _  )    = LaTeX   beginEnd
-setBegin beginEnd (OrgMode _ lang) = OrgMode beginEnd lang
-setBegin beginEnd (Jekyll  _ lang) = Jekyll  beginEnd lang
-setBegin _         del             = del
+setBegin beginEnd (LaTeX    _  )    = LaTeX    beginEnd
+setBegin beginEnd (OrgMode  _ lang) = OrgMode  beginEnd lang
+setBegin beginEnd (Jekyll   _ lang) = Jekyll   beginEnd lang
+setBegin beginEnd (Asciidoc _ lang) = Asciidoc beginEnd lang
+setBegin _         del              = del
 
 data Fence
   = Tilde
@@ -66,6 +69,8 @@ emitDelimiter (OrgMode End _)       = "#+END_SRC"
 emitDelimiter  Bird                 = ">"
 emitDelimiter (Jekyll Begin l)      = "{% highlight" <+> fromMaybe "" l <+> "%}"
 emitDelimiter (Jekyll End   _)      = "{% endhighlight %}"
+emitDelimiter (Asciidoc Begin l)    = "[source" <> maybe "" (", "<>) l <> "]\n----"
+emitDelimiter (Asciidoc End   _)    = "----"
 emitDelimiter (Markdown Tilde l)    = "~~~" <+> fromMaybe "" l
 emitDelimiter (Markdown Backtick l) = "```" <+> fromMaybe "" l
 
@@ -114,6 +119,18 @@ isMarkdown fence fenceStr lang l
     Just $ Markdown fence $ bool Nothing lang (l `containsLang` lang)
   | otherwise = Nothing
 
+isAsciidoc :: Lang -> Recogniser
+isAsciidoc lang l
+  | "[source" `isPrefixOf` l
+    && l `containsLang` lang
+    && "]" `isSuffixOf` stripEnd l = Just $ Asciidoc Begin lang
+  | "----" `isPrefixOf` l          = Just $ Asciidoc End   lang
+  | otherwise                      = Nothing
+
+asciidocFence :: [(Int,String)] -> Maybe [(Int,String)]
+asciidocFence ls | ((_,"----"):ls') <- ls = Just ls'
+                 | otherwise              = Nothing
+
 isDelimiter :: Style -> Recogniser
 isDelimiter ds l = asum (map go ds)
   where
@@ -123,19 +140,22 @@ isDelimiter ds l = asum (map go ds)
     go (Markdown Tilde lang)    = isMarkdown Tilde "~~~" lang l
     go (Markdown Backtick lang) = isMarkdown Backtick "```" lang l
     go (OrgMode _ lang)         = isOrgMode lang l
+    go (Asciidoc _ lang)        = isAsciidoc lang l
 
 match :: Delimiter -> Delimiter -> Bool
-match (LaTeX Begin)     (LaTeX End)             = True
-match (Jekyll Begin _)  (Jekyll End _)          = True
-match (OrgMode Begin _) (OrgMode End _)         = True
-match (Markdown f _)    (Markdown g Nothing)    = f == g
-match  _                 _                      = False
+match (LaTeX Begin)      (LaTeX End)          = True
+match (Jekyll Begin _)   (Jekyll End _)       = True
+match (OrgMode Begin _)  (OrgMode End _)      = True
+match (Asciidoc Begin _) (Asciidoc End _)     = True
+match (Markdown f _)     (Markdown g Nothing) = f == g
+match  _                  _                   = False
 
 type Style = [Delimiter]
 
-all, backtickfence, bird, haskell, infer, jekyll, latex, markdown, orgmode, tildefence :: Style
-all           = latex <> markdown <> orgmode <> jekyll
+all, backtickfence, tildefence, bird, haskell, infer, jekyll, latex, markdown, orgmode, asciidoc :: Style
+all           = latex <> markdown <> orgmode <> jekyll <> asciidoc
 backtickfence = [Markdown Backtick Nothing]
+tildefence    = [Markdown Tilde Nothing]
 bird          = [Bird]
 haskell       = latex <> bird
 infer         = []
@@ -143,7 +163,7 @@ jekyll        = [Jekyll Begin Nothing, Jekyll End Nothing]
 latex         = [LaTeX Begin, LaTeX End]
 markdown      = bird <> tildefence <> backtickfence
 orgmode       = [OrgMode Begin Nothing, OrgMode End Nothing]
-tildefence    = [Markdown Tilde Nothing]
+asciidoc      = [Asciidoc Begin Nothing, Asciidoc End Nothing]
 
 parseStyle :: String -> Maybe Style
 parseStyle s = case toLower s of
@@ -156,6 +176,7 @@ parseStyle s = case toLower s of
   "latex"         -> Just latex
   "markdown"      -> Just markdown
   "orgmode"       -> Just orgmode
+  "asciidoc"      -> Just asciidoc
   "tildefence"    -> Just tildefence
   _               -> Nothing
 
@@ -169,11 +190,12 @@ setLang' lang (Jekyll beginEnd _)  = Jekyll beginEnd lang
 setLang' _     d                   = d
 
 inferred :: Maybe Delimiter -> Style
-inferred  Nothing             = []
-inferred (Just (LaTeX _))     = latex
-inferred (Just (Jekyll _ _))  = jekyll
-inferred (Just (OrgMode _ _)) = orgmode
-inferred (Just _)             = markdown
+inferred  Nothing              = []
+inferred (Just (LaTeX _))      = latex
+inferred (Just (Jekyll _ _))   = jekyll
+inferred (Just (OrgMode _ _))  = orgmode
+inferred (Just (Asciidoc _ _)) = asciidoc
+inferred (Just _)              = markdown
 
 data WhitespaceMode
   = WsKeepIndent -- ^ keeps only indentations
@@ -201,30 +223,34 @@ unlit' _ _ (Just Bird) []  = Right []
 unlit' _ _ (Just o)    []  = Left $ UnexpectedEnd o
 unlit' ws ss q ((n, l):ls) = case (q, q') of
 
-  (Nothing  , Nothing)   -> continue $ lineIfKeepAll
+  (Nothing  , Nothing)   -> continue  $ lineIfKeepAll
 
-  (Just Bird, Nothing)   -> close    $ lineIfKeepAll
-  (Just _o  , Nothing)   -> continue $ [l]
+  (Just Bird, Nothing)   -> close     $ lineIfKeepAll
+  (Just _o  , Nothing)   -> continue  $ [l]
 
-  (Nothing  , Just Bird) -> open     $ lineIfKeepIndent <> [stripBird' ws l]
+  (Nothing  , Just Bird) -> open      $ lineIfKeepIndent <> [stripBird' ws l]
+  (Nothing  , Just (Asciidoc Begin _))
+    | Just ls' <- asciidocFence ls
+                         -> open' ls' $ lineIfKeepAll <> lineIfKeepIndent
   (Nothing  , Just c)
-     | isBegin c         -> open     $ lineIfKeepAll <> lineIfKeepIndent
-     | otherwise         -> Left     $ SpuriousDelimiter n c
+    | isBegin c          -> open      $ lineIfKeepAll <> lineIfKeepIndent
+    | otherwise          -> Left      $ SpuriousDelimiter n c
 
-  (Just Bird, Just Bird) -> continue $ [stripBird' ws l]
-  (Just _o  , Just Bird) -> continue $ [l]
+  (Just Bird, Just Bird) -> continue  $ [stripBird' ws l]
+  (Just _o  , Just Bird) -> continue  $ [l]
   (Just o   , Just c)
-     | o `match` c       -> close    $ lineIfKeepAll
-     | otherwise         -> Left     $ SpuriousDelimiter n c
+    | o `match` c        -> close     $ lineIfKeepAll
+    | otherwise          -> Left      $ SpuriousDelimiter n c
 
   where
-    q'                = isDelimiter (ss `or` all) l
-    continueWith r l' = (l' <>) <$> unlit' ws (ss `or` inferred q') r ls
-    open              = continueWith q'
-    continue          = continueWith q
-    close             = continueWith Nothing
-    lineIfKeepAll     = case ws of WsKeepAll    -> [""]; WsKeepIndent -> []
-    lineIfKeepIndent  = case ws of WsKeepIndent -> [""]; WsKeepAll -> []
+    q'                    = isDelimiter (ss `or` all) l
+    continueWith r ls' l' = (l' <>) <$> unlit' ws (ss `or` inferred q') r ls'
+    open' ls'             = continueWith q' ls'
+    open                  = open' ls
+    continue              = continueWith q ls
+    close                 = continueWith Nothing ls
+    lineIfKeepAll         = case ws of WsKeepAll    -> [""]; WsKeepIndent -> []
+    lineIfKeepIndent      = case ws of WsKeepIndent -> [""]; WsKeepAll -> []
 
 relit :: Style -> Delimiter -> String -> Either Error String
 relit ss ts = fmap unlines . relit' ss ts Nothing . zip [1..] . lines
@@ -254,6 +280,9 @@ relit' ss ts q ((n, l):ls) = case (q, q') of
   (Nothing  , Nothing)   -> continue
 
   (Nothing  , Just Bird) -> blockOpen $ Just (stripBird l)
+  (Nothing  , Just (Asciidoc Begin _))
+    | Just ls' <- asciidocFence ls
+                         -> blockOpen' ls' Nothing
   (Nothing  , Just c)
     | isBegin c          -> blockOpen Nothing
     | otherwise          -> Left $ SpuriousDelimiter n c
@@ -268,12 +297,13 @@ relit' ss ts q ((n, l):ls) = case (q, q') of
     | otherwise          -> Left $ SpuriousDelimiter n c
 
   where
-    q'               = isDelimiter (ss `or` all) l
-    continueWith  r  = relit' (ss `or` inferred q') ts r ls
-    continue         = (l :)                <$> continueWith q
-    blockOpen     l' = (emitOpen  ts l' <>) <$> continueWith q'
-    blockContinue l' = (emitCode  ts l' :)  <$> continueWith q
-    blockClose l'    = (emitClose ts l' <>)  <$> continueWith Nothing
+    q'                 = isDelimiter (ss `or` all) l
+    continueWith r ls' = relit' (ss `or` inferred q') ts r ls'
+    continue           = (l :)                <$> continueWith q ls
+    blockOpen' ls' l'  = (emitOpen  ts l' <>) <$> continueWith q' ls'
+    blockOpen      l'  = blockOpen' ls l'
+    blockContinue  l'  = (emitCode  ts l' :)  <$> continueWith q ls
+    blockClose     l'  = (emitClose ts l' <>) <$> continueWith Nothing ls
 
 data Error
   = SpuriousDelimiter Int Delimiter
